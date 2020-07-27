c8d5569cb130117918ccb67381c954ae7d2dd776
akhil chowdary <akhilchowdary.m007@gmail.com>
2020-07-27 18:51:17 +0530

    delete child

--- myapp/views.py
+++ myapp/views.py
@@ -9,5 +9,3 @@ def home(request):
 def fact(request,n):
     n=int(n)
     return HttpResponse("<h3>Factorial is {}</h3>".format(factorial(n)))
-def child(request):
-    return render(request,"child.html")
